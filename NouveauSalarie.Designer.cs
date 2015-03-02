31938256285b846792d2760273ff8b0efeae12f4
corentinOblin <exeloir@hotmail.fr>
2015-03-02 17:50:35 +0100

    Nouveau Salarié

---
+++ NouveauSalarie.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace PTOuatelse
+{
+    partial class NouveauSalarie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NouveauSalarie));
+            this.Mail = new System.Windows.Forms.TextBox();
+            this.DateNaissance = new System.Windows.Forms.TextBox();
+            this.Téléphone = new System.Windows.Forms.TextBox();
+            this.NomPays = new System.Windows.Forms.TextBox();
+            this.NomVille = new System.Windows.Forms.TextBox();
+            this.CodePostal = new System.Windows.Forms.TextBox();
+            this.Adresse2 = new System.Windows.Forms.TextBox();
+            this.Adresse1 = new System.Windows.Forms.TextBox();
+            this.CodeSalarie = new System.Windows.Forms.TextBox();
+            this.Nom = new System.Windows.Forms.TextBox();
+            this.Genre = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Mail
+            // 
+            this.Mail.Location = new System.Drawing.Point(149, 278);
+            this.Mail.Name = "Mail";
+            this.Mail.Size = new System.Drawing.Size(672, 20);
+            this.Mail.TabIndex = 86;
+            // 
+            // DateNaissance
+            // 
+            this.DateNaissance.Location = new System.Drawing.Point(634, 246);
+            this.DateNaissance.Name = "DateNaissance";
+            this.DateNaissance.Size = new System.Drawing.Size(188, 20);
+            this.DateNaissance.TabIndex = 85;
+            // 
+            // Téléphone
+            // 
+            this.Téléphone.Location = new System.Drawing.Point(149, 246);
+            this.Téléphone.Name = "Téléphone";
+            this.Téléphone.Size = new System.Drawing.Size(369, 20);
+            this.Téléphone.TabIndex = 84;
+            // 
+            // NomPays
+            // 
+            this.NomPays.Location = new System.Drawing.Point(566, 214);
+            this.NomPays.Name = "NomPays";
+            this.NomPays.Size = new System.Drawing.Size(255, 20);
+            this.NomPays.TabIndex = 83;
+            // 
+            // NomVille
+            // 
+            this.NomVille.Location = new System.Drawing.Point(256, 214);
+            this.NomVille.Name = "NomVille";
+            this.NomVille.Size = new System.Drawing.Size(262, 20);
+            this.NomVille.TabIndex = 82;
+            // 
+            // CodePostal
+            // 
+            this.CodePostal.Location = new System.Drawing.Point(149, 214);
+            this.CodePostal.Name = "CodePostal";
+            this.CodePostal.Size = new System.Drawing.Size(100, 20);
+            this.CodePostal.TabIndex = 81;
+            // 
+            // Adresse2
+            // 
+            this.Adresse2.Location = new System.Drawing.Point(149, 181);
+            this.Adresse2.Name = "Adresse2";
+            this.Adresse2.Size = new System.Drawing.Size(673, 20);
+            this.Adresse2.TabIndex = 80;
+            // 
+            // Adresse1
+            // 
+            this.Adresse1.Location = new System.Drawing.Point(149, 155);
+            this.Adresse1.Name = "Adresse1";
+            this.Adresse1.Size = new System.Drawing.Size(673, 20);
+            this.Adresse1.TabIndex = 79;
+            // 
+            // CodeSalarie
+            // 
+            this.CodeSalarie.Location = new System.Drawing.Point(149, 128);
+            this.CodeSalarie.Name = "CodeSalarie";
+            this.CodeSalarie.Size = new System.Drawing.Size(238, 20);
+            this.CodeSalarie.TabIndex = 78;
+            // 
+            // Nom
+            // 
+            this.Nom.Location = new System.Drawing.Point(536, 130);
+            this.Nom.Name = "Nom";
+            this.Nom.Size = new System.Drawing.Size(286, 20);
+            this.Nom.TabIndex = 77;
+            // 
+            // Genre
+            // 
+            this.Genre.FormattingEnabled = true;
+            this.Genre.Location = new System.Drawing.Point(473, 129);
+            this.Genre.Name = "Genre";
+            this.Genre.Size = new System.Drawing.Size(57, 21);
+            this.Genre.TabIndex = 76;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(393, 132);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(74, 13);
+            this.label9.TabIndex = 75;
+            this.label9.Text = "Nom Prénom :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(71, 281);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 74;
+            this.label8.Text = "Courriel :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(70, 249);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 72;
+            this.label6.Text = "Tel/Port :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(524, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 71;
+            this.label5.Text = "Pays :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 70;
+            this.label4.Text = "Ville :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 69;
+            this.label3.Text = "Adresse :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 68;
+            this.label2.Text = "Code salarié :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(18, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 96);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 67;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(143, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 31);
+            this.label1.TabIndex = 66;
+            this.label1.Text = "Détails d\'un Salarié";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(524, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(102, 13);
+            this.label7.TabIndex = 73;
+            this.label7.Text = "Date de naissance :";
+            // 
+            // NouveauSalarie
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 353);
+            this.Controls.Add(this.Mail);
+            this.Controls.Add(this.DateNaissance);
+            this.Controls.Add(this.Téléphone);
+            this.Controls.Add(this.NomPays);
+            this.Controls.Add(this.NomVille);
+            this.Controls.Add(this.CodePostal);
+            this.Controls.Add(this.Adresse2);
+            this.Controls.Add(this.Adresse1);
+            this.Controls.Add(this.CodeSalarie);
+            this.Controls.Add(this.Nom);
+            this.Controls.Add(this.Genre);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label7);
+            this.Name = "NouveauSalarie";
+            this.Text = "NouveauSalarie";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Mail;
+        private System.Windows.Forms.TextBox DateNaissance;
+        private System.Windows.Forms.TextBox Téléphone;
+        private System.Windows.Forms.TextBox NomPays;
+        private System.Windows.Forms.TextBox NomVille;
+        private System.Windows.Forms.TextBox CodePostal;
+        private System.Windows.Forms.TextBox Adresse2;
+        private System.Windows.Forms.TextBox Adresse1;
+        private System.Windows.Forms.TextBox CodeSalarie;
+        private System.Windows.Forms.TextBox Nom;
+        private System.Windows.Forms.ComboBox Genre;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+
+    }
+}
