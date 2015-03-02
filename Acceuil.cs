6421351713e7063f5fbc85a735a93a08cd57009b
Antdub <a_nthony64@live.fr>
2015-03-02 17:53:43 +0100

    Gestion salariés depuis l'accueil

--- Acceuil.cs
+++ Acceuil.cs
@@ -43,7 +43,8 @@ namespace WindowsFormsApplication1
 
         private void SalarieButton_Click(object sender, EventArgs e)
         {
-
+            GestionSalarie salarié = new GestionSalarie();
+            salarié.Show();
         }
 
         private void AssistanceButton_Click(object sender, EventArgs e)
