81438b7680f476b565bdfa6247a00bc794612565
sorrow33 <william.sireix@gmail.com>
2015-03-19 17:00:15 +0100

    list des salariés

--- GestionSalarie.cs
+++ GestionSalarie.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,9 +12,18 @@ using System.Windows.Forms;
 
 namespace PTOuatelse
 {
+
+
     public partial class GestionSalarie : Form
     {
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        private MySqlConnection connection;
+        private MySqlDataAdapter mySqlDataAdapter;
         string chaine = null;
+
         public GestionSalarie()
         {
             InitializeComponent();
@@ -20,7 +31,11 @@ namespace PTOuatelse
             connexion.Initialize();
             chaine = connexion.requete("SELECT nom FROM  salaries", "nom");
             MessageBox.Show(chaine.ToString());
+
+
+
         }
+
 
         private void NouveauSalarie_Click(object sender, EventArgs e)
         {
@@ -41,7 +56,22 @@ namespace PTOuatelse
 
         private void GestionSalarie_Load(object sender, EventArgs e)
         {
+            server = "info-viviane.iut.bx1";
+            database = "pt_ouatelse1";
+            uid = "pt_ouatelse1";
+            password = "DYwBqVhnNL5SPddK";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+
+            mySqlDataAdapter = new MySqlDataAdapter("select identifiant,nom,prenom,adresse1 from salaries", connection);
+            DataSet ds = new DataSet();
+            mySqlDataAdapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
 
         }
+        
+
     }
 }
