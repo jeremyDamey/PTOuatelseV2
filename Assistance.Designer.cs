6e3dbcfa8e5dbe58184f2a13831ee7739e3065af
corentinOblin <exeloir@hotmail.fr>
2015-03-23 16:25:29 +0100

    ajout de l'assistance

---
+++ Assistance.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace PTOuatelse
+{
+    partial class Assistance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.OKAssistance = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nous contacter :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(213, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Corentin OBLIN : corentin.oblin@ouatelse.fr";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Anthony DUBERTRAND : anthony.dubertrand@ouatelse.fr";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(104, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Maxime BOISSE : maxime.boisse@ouatelse.fr";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(104, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(198, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "William SIREX : william.sirex@ouatelse.fr";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(104, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(214, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Jeremy DAMEY : jeremy.damey@ouatelse.fr";
+            // 
+            // OKAssistance
+            // 
+            this.OKAssistance.Location = new System.Drawing.Point(152, 217);
+            this.OKAssistance.Name = "OKAssistance";
+            this.OKAssistance.Size = new System.Drawing.Size(75, 23);
+            this.OKAssistance.TabIndex = 6;
+            this.OKAssistance.Text = "OK";
+            this.OKAssistance.UseVisualStyleBackColor = true;
+            this.OKAssistance.Click += new System.EventHandler(this.OKAssistance_Click);
+            // 
+            // Assistance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 252);
+            this.Controls.Add(this.OKAssistance);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Assistance";
+            this.Text = "Assistance";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button OKAssistance;
+    }
+}
